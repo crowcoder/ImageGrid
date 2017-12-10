43aa9db621ee912dbcd12df5b30be52633fee338
Bob <crowcoder@gmail.com>
2017-12-10 08:49:06 -0500

    Add project files.

---
+++ ImageGrid/FilesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace ImageGrid
+{
+    class FilesViewModel
+    {
+        public string FileName { get; set; }
+        public Image Picture { get; set; }
+    }
+}
---
+++ ImageGrid/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImageGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindingList<FilesViewModel> vmList = new BindingList<FilesViewModel>();
+
+            string[] theFiles = Directory.GetFiles(Path.Combine(Environment.CurrentDirectory, "images"));
+
+            foreach (string f in theFiles)
+            {
+                Image img = Image.FromFile(f);
+                vmList.Add(new FilesViewModel { FileName = Path.GetFileName(f), Picture = img });
+            }
+
+            dataGridView1.DataSource = vmList;
+        }
+    }
+}
